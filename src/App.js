186c175837830a2782974a7fb42f41a2c305f046
class1 <class1@gmail.com>
2024-02-22 11:16:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './components/home';
+import Navbar from './components/navbar';
+
+// JavaScript code
+
+
+
+
+
+
+
+
+
+function App() {
+  return (
+
+    <div className = "App" >
+      <Navbar / >
+    <HomePage / >
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,62 @@
+import React, { Component, useState } from 'react';
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {AiOutlineClose} from 'react-icons/ai';
+import './style.css'
+
+const Navbar = () => {
+
+  //Phone call
+  const phoneNumber = '+1234567890';
+
+  const handleCallClick = () => {
+    window.location.href = `tel:${phoneNumber}`;
+  };
+
+
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+  setMenuVisible(!menuVisible);
+  };
+
+
+
+  // scrolling 
+  // const handleNavLinkClick = (event) => {
+  //   event.preventDefault();
+  //   // Handle your navigation logic here
+  // };
+  const handleNavLinkClick = (event, targetId) => {
+    event.preventDefault();
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+    setMenuVisible(!menuVisible)
+  };
+
+
+
+  return (
+    <div className='nav-div'>
+      <div className='logo'><p>Logo</p></div>
+
+      <div className={`menu-items ${menuVisible ? 'active' : ''}`}>
+        <ul>
+          <li><a href="#" className='nav-link' onClick={(e) => handleNavLinkClick(e, 'content1')}>Home</a></li>
+          
+          <li><a href="#" className='nav-link'  onClick={(e) => handleNavLinkClick(e, 'content2')}>Services</a></li>
+          <li><a href="#" className='nav-link'  onClick={(e) => handleNavLinkClick(e, 'content7')} >Gallery</a></li>
+          <li><a href="#" className='nav-link'  onClick={(e) => handleNavLinkClick(e, 'content3')}>About Us</a></li>
+        </ul>
+      </div>
+      <div className='contact-btn' onClick={handleCallClick}><p> {phoneNumber}</p></div>
+      <div className='menu-toggle-btn' onClick={toggleMenu} >{menuVisible ?  <AiOutlineClose/>: <GiHamburgerMenu/> }</div>
+
+    </div>
+  );
+
+};
+
+export default Navbar;
+
